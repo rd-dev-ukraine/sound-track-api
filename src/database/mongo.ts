78c4406e56b76d461336e57160671d460ff83422
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2017-12-11 13:11:03 +0200

    Changed database name to sound-track

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -5,7 +5,7 @@ let db: Db;
 export async function connectToDb(): Promise<Db> {
     const connectionUri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}`
         + `@cluster0-shard-00-00-nh12c.mongodb.net:27017,cluster0-shard-00-01-nh12c.mongodb.net:27017`
-        + `,cluster0-shard-00-02-nh12c.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
+        + `,cluster0-shard-00-02-nh12c.mongodb.net:27017/sound-track?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
     return MongoClient.connect(connectionUri).then(database => {
         db = database;
         return database;
