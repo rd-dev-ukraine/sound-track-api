1d145c6289191e1ec7f0986dd8353c3da236a8c0
Alex Kononykhin <aakononykhin@gmail.com>
2017-12-21 11:13:05 +0200

    Added auth token to allowed headers

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const router = express.Router();
 //Allow cross origin requests
 app.use(function (_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth-Token");
     next();
 });
 
